26959e06451e2f9479c32d3069fc9713ac1704c3
Matt Gautreau <mgaut72@gmail.com>
2015-05-04 11:20:07 -0700

    add NICK message execute functionality

--- src/main/java/com/zachmatt/irc/messages/NickMessage.java
+++ src/main/java/com/zachmatt/irc/messages/NickMessage.java
@@ -13,7 +13,40 @@ public class NickMessage extends Message {
     }
 
 
-    public List<String> executeCommand(Server server, UserInfo user) {
-        return null;
+    public List<String> executeCommand(Server server, UserInfo u) {
+        String oldNick = u.nickname;
+
+        // make sure we were given a nickname
+        try{
+            String newNick = this.params.get(0);
+        }
+        catch(IndexOutOfBoundsException e){
+            return super.generateResponse(ResponseCode.ERR_NONICKNAMEGIVEN, u);
+        }
+
+        // make sure the new nickname doesn't already exist
+        try{
+            UserInfo existing = server.getUserByNickname(newNick);
+            return super.generateResponse(ResponseCode.ERR_NICKNAMEINUSE, u);
+        }
+        catch(UserNotFoundException e){
+            // expected to not find the user, so ok
+        }
+
+
+        // user must register and send PASS command first
+        if(u.registrationState == UserInfo.RegistrationState.NONE_SENT){
+            return super.generateResponse(ResponseCode.ERR_NOTREGISTERED, u);
+        }
+        // if they have never set a nickname, update their registration state
+        else if(u.registrationState == UserInfo.RegistrationState.PASS_SENT){
+            u.registrationState = UserInfo.RegistrationState.NICK_SENT;
+        }
+
+        // finally, set the new nickname
+        u.nickname = newNick;
+        return new ArrayList<String>() {{
+            add(":" + oldNick + " NICK " + newNick);
+        }};
     }
 }
